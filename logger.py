7609779d5d4071907fc2305990d95b8086aa3d23
Aaron Tan <a.tan@keio.jp>
2018-06-04 11:19:27 +0900

    Added function to log both CPU and Memory usage

--- logger.py
+++ logger.py
@@ -21,8 +21,8 @@ def get_processes():
 def get_cpu_usage(proc_list):
     cpu_percent = 0
     for proc in proc_list:
-        cpu_percent += proc.cpu_percent(interval=1.0)
-    print cpu_percent
+        cpu_percent += proc.cpu_percent(interval=0)
+    return cpu_percent
 
 
 def get_memory_usage(proc_list):
@@ -37,14 +37,25 @@ def log_memory_usage(proc_list):
         maximum = 0
         total = 0
         ticks = 0
-        while True:
-            time.sleep(1)
-            mem = get_memory_usage(proc_list)
-            total += float(mem)
-            ticks += 1
-            if mem > maximum:
-                maximum = mem
-            print "Total memory usage is: {}%".format(mem)
+        fname = ""
+        for proc in proc_list:
+            name = proc.name()
+            if "Electron" in name:
+                fname = "vscode"
+            elif "atom" in name.lower():
+                fname = "atom"
+            elif "Sublime" in name:
+                fname = "sublime"
+        with open("./logs/memory_log_{}.csv".format(fname), "w") as f:
+            while True:
+                time.sleep(1)
+                mem = get_memory_usage(proc_list)
+                total += float(mem)
+                ticks += 1
+                if mem > maximum:
+                    maximum = mem
+                f.write(str(mem)+"\n")
+                print "Total memory usage is: {}%".format(mem)
     except KeyboardInterrupt:   
         print "Maximum reached memory usage was  : {}%".format(maximum)
         print "Average memory percentage used is: {}%".format(total/ticks)
@@ -56,7 +67,16 @@ def log_cpu_usage(proc_list):
         maximum = 0
         total = 0
         ticks = 0
-        with open(mem_log.txt, "w") as f:
+        fname = ""
+        for proc in proc_list:
+            name = proc.name()
+            if "Electron" in name:
+                fname = "vscode"
+            elif "atom" in name.lower():
+                fname = "atom"
+            elif "Sublime" in name:
+                fname = "sublime"
+        with open("cpu_usage_{}.csv".format(name), "w") as f:
             while True:
                 time.sleep(1)
                 cpu = get_cpu_usage(proc_list)
@@ -64,12 +84,51 @@ def log_cpu_usage(proc_list):
                 ticks += 1
                 if cpu > maximum:
                     maximum = cpu
-                print "Total cpu usage is: {}%".format(mem)
-                f.write(mem)
+                f.write(str(cpu)+"\n")
+                print "Total cpu usage is: {}%".format(cpu)
     except KeyboardInterrupt:
         print "\nMaximum reached cpu usage was  : {}%".format(maximum)
         print "Average memory percentage used is: {}%".format(total/ticks)
         return
 
 
+def log_all(proc_list):
+    try:
+        max_mem = 0
+        max_cpu = 0
+        total_mem = 0
+        total_cpu = 0
+        ticks = 0
+        fname = ""
+        for proc in proc_list:
+            name = proc.name()
+            if "Electron" in name:
+                fname = "vscode"
+            elif "atom" in name.lower():
+                fname = "atom"
+            elif "Sublime" in name:
+                fname = "sublime"
+        with open("./logs/full_log_{}.csv".format(fname), "w") as f:
+            f.write("cpu percent, memory percent\n")
+            while True:
+                time.sleep(1)
+                cpu = get_cpu_usage(proc_list)
+                mem = get_memory_usage(proc_list)
+                total_cpu += cpu
+                total_mem += mem
+                ticks += 1
+                if cpu > max_cpu:
+                    max_cpu = cpu
+                if mem > max_mem:
+                    max_mem = mem
+                f.write("{},{}\n".format(cpu, mem))
+                print "Total cpu usage is: {}%".format(cpu)
+                print "Total mem usage is: {}%".format(mem)
+            return
+    except:
+        print "\nMaximum reached cpu usage was   : {}%".format(max_cpu)
+        print "Average cpu percentage used was   : {}%".format(total_cpu/ticks)
+        print "\nMaximum reached memory usage was: {}%".format(max_mem)
+        print "Average memory percentage used was: {}%".format(total_mem/ticks)
+        print("done")
 get_processes()
